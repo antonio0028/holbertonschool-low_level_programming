e304f6440ffa2745b55ee927ba3ebacccfb980af
antonio0028 <2778@holbertonschool.com>
2021-02-06 01:58:05 +0000

    added new 8th .c file task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - ZtoA
+ * Return: 0
+ */
+int main(void)
+{
+	int ZtoA;
+
+	for (ZtoA = 122; ZtoA >= 97; ZtoA--)
+		putchar(ZtoA);
+	putchar('\n');
+	return (0);
+}
