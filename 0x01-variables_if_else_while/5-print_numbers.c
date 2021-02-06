6a4af5ad78aa903ab56ae6ca0801bc0232945dc3
antonio0028 <2778@holbertonschool.com>
2021-02-06 01:06:34 +0000

    added 6th .c file task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - numbers
+ * Return: (0)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		printf("%d", numbers);
+	}
+	printf("\n");
+	return (0);
+}
