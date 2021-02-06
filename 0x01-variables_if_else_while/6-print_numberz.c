013d991d9ac0e7ed4521b04c70ff9bfa2885ae62
antonio0028 <2778@holbertonschool.com>
2021-02-06 01:28:49 +0000

    added 7th new .c file task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - numberz
+ * Return: 0
+ */
+int main(void)
+{
+	int numberz;
+
+	for (numberz = 48; numberz < 58; numberz++)
+	{
+		putchar(numberz);
+	}
+	putchar('\n');
+	return (0);
+}
