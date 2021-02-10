028b9f7493c103c4d000e760436e6d9112bdf83b
antonio0028 <2778@holbertonschool.com>
2021-02-10 04:22:17 +0000

    completed 5th .c file task function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_sign - sign of num
+ *@n: the num to inspect
+ *Return: 1 if greater than zero. 0 if @n is zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+return (-1);
+}
