21e7f3845bad2ac7714cbdeb906e9679ac427db3
antonio0028 <2778@holbertonschool.com>
2021-02-12 01:16:56 +0000

    completed new 1st .c file task function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isupper - check for uppercase
+ * @c: uppercase
+ * Return: 1 if c is uppercase. Zero if not.
+ */
+int _isupper(int c)
+{
+	return ((c >= 65) && (c <= 90));
+}
