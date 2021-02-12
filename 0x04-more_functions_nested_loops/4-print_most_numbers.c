02707906e6a02ee9cf579dca357efe2422c27088
antonio0028 <2778@holbertonschool.com>
2021-02-12 14:41:45 +0000

    completed 4th .c file task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - except 2 & 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if ((num != '2') && (num != '4'))
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
