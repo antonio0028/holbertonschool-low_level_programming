36f2cb9b2f5e41a2d320c9445e02da1bbf63ca16
antonio0028 <2778@holbertonschool.com>
2021-02-19 18:07:33 +0000

    completed 1st string file task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcat - link 2 strings
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int num1;
+	int num2;
+
+	for (num1 = 0; dest[num1] != '\0'; num1++)
+	{
+		;
+	}
+	
+	for (num2 = 0; src[num2] != '\0'; num2++)
+	{
+		dest[num1] = src[num2];
+		num1++;
+	}
+	
+	dest[num1] = '\0';
+
+	return (dest);
+}
